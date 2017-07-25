e7195b329d4b1cac0cc4dfa8db6bfda78f222a9d
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2017-07-25 18:45:14 -0300

    Readonly all fields

--- server/journals/admin.py
+++ server/journals/admin.py
@@ -12,6 +12,12 @@ class DocumentsAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['show_save_and_continue'] = False
+        extra_context['show_save'] = False
+        return super(YourModelAdmin, self).changeform_view(request, object_id extra_context=extra_context)
+
 
 @admin.register(Rawdata)
 class RawdataAdmin(admin.ModelAdmin):
