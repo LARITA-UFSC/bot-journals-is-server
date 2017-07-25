f1dac686498ccbda8837bc5b966c611a7dcbc729
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2017-07-25 18:30:47 -0300

    Readonly all fields

--- server/journals/admin.py
+++ server/journals/admin.py
@@ -2,21 +2,14 @@ from django.contrib import admin
 from .models import Documents, Rawdata
 
 
-class CustomPropertiesMixinAdmin():
-
-    def get_readonly_fields(self, request, obj=None):
-        if obj:
-            self.readonly_fields = [
-                field.name for field in obj.__class__._meta.get_fields()]
-        return self.readonly_fields
-
-
-@admin.register(Documents)
-class DocumentsAdmin(CustomPropertiesMixinAdmin, admin.ModelAdmin):
+@admin.register(CustomPropertiesMixinAdmin, Documents)
+class DocumentsAdmin(admin.ModelAdmin, CustomPropertiesMixinAdmin):
     list_filter = ['journal']
     search_fields = ['title', 'keywords', 'summary']
+    readonly_fields = ['title', 'authors', 'keywords',
+                       'summary', 'url_view', 'url_pdf', 'journal']
 
 
-@admin.register(Rawdata)
-class RawdataAdmin(admin.ModelAdmin):
+@admin.register(CustomPropertiesMixinAdmin, Rawdata)
+class RawdataAdmin(admin.ModelAdmin, CustomPropertiesMixinAdmin):
     pass
