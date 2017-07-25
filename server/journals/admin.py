c8c20fe429f20c67a080babcb0faa6ac820ea9d2
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2017-07-25 17:46:29 -0300

    Incluído filtros

--- server/journals/admin.py
+++ server/journals/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from .models import Documents, Rawdata
 
 
+class CustomPropertiesMixinAdmin():
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            self.readonly_fields = [
+                field.name for field in obj.__class__._meta.get_fields()]
+        return self.readonly_fields
+
+
 @admin.register(Documents)
-class DocumentsAdmin(admin.ModelAdmin):
-    pass
+class DocumentsAdmin(admin.ModelAdmin, CustomPropertiesMixinAdmin):
+    list_filter = ('title', 'summary', 'journal')
 
 
 @admin.register(Rawdata)
-class RawdataAdmin(admin.ModelAdmin):
+class RawdataAdmin(admin.ModelAdmin, CustomPropertiesMixinAdmin):
     pass
