32a68bdb716e64b8e7ecdb3f0daf79780d5cb9d9
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2017-09-02 18:16:21 -0300

    Fix static files Django Admin

--- server/server/settings.py
+++ server/server/settings.py
@@ -138,11 +138,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 REST_FRAMEWORK = {
