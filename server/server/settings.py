8f07f57e28675471110c9cad82638cfc87efbee7
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2017-09-02 17:50:17 -0300

    DEBUG definido como false

--- server/server/settings.py
+++ server/server/settings.py
@@ -23,9 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f-!_b5p4etg(^pl&7^$ql00pw&ef!d1)g%%z#uuy!_ay2k-m74'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True) 
-
-print(DEBUG)
+DEBUG = os.environ.get('DEBUG', False) 
 
 ALLOWED_HOSTS = ['*']
 
