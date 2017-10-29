7b3089ac86d922a5382bafac96a154b6c28d51ad
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2017-10-29 17:00:56 -0200

    Incluíudo arquivo .env para definir as variáveis de ambiente

--- server/server/settings.py
+++ server/server/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import dj_database_url
 import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+dotenv_file = os.path.join(BASE_DIR, '..', '.env') 
+load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
