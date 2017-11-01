9e75a582ee47cc62b1bf27873426431f1ef40910
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2017-11-01 16:01:44 -0200

    Incluido nova variavel de ambiente

--- server/server/settings.py
+++ server/server/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(env='DATABASE_URL_AMAZON', conn_max_age=600)
 }
 # DATABASES = {
 # 'default': {
